177f5f43ff7ab191854800b2c2b2ae7affee09cd
LuceroG <ml17ft@gmail.com>
2017-03-09 11:57:02 -0600

    eventos on

--- holajquery.js
+++ holajquery.js
@@ -3,9 +3,14 @@ function saluda()
 {
     alert($('h1').text())
 }*/
-$(function()
+/*$(function()
 {
     //alert($('h1').text)
     $nuevo = $('<p>parrafo Nuevo</p>')
     $('h2').after($nuevo)
-})
+    /*$('#boton1').click(function (e) { 
+        e.
+        e.preventDefault();
+        
+    });
+})*/
